2283c662b1210c05ef1878c99eeea8416e846887
YonaBMoreda <y.moreda@student.rug.nl>
2020-04-30 00:09:10 +0200

    virus total support in contextual feats

--- src/main.py
+++ src/main.py
@@ -1,28 +1,34 @@
 import os
 from androguard.misc import AnalyzeAPK
 from androguard.core.analysis import analysis
-from manifest_analysis import *
-from sourcecode_analysis import *
+from src.contextual_feat_extraction import run_contextual
+
+# from manifest_analysis import *
+# from sourcecode_analysis import *
+
 
 def analyzeDex(d, dx):
     print("TODO\n")
 
+
 # This function retrieves information about the APK itself.
-# We analyize the manifest file using androguard functions and return any relevant information.
+# We analyze the manifest file using androGuard functions and return any relevant information.
 def analyzeAPK(a):
     print(a.get_app_name())
     print(a.get_permissions())
     print(a.get_activities())
     print(a.get_android_resources())
 
-def main():
-    #a : APK object; We can obtain all information about the APK here (manifest file stuff)
-    #d : array of DalvikVMFormat objects; Corresponds to the DEX file. We can obtain classes, methods, strings etc. from here.
-    #dx : Analysis object; Contains special classes, which link information about classes.dex and can handle multiple DEX files. (Extension of d object basically)
-    a, d, dx = AnalyzeAPK("./apks/WhatsApp.apk")
 
+def main():
+    # a : APK object; We can obtain all information about the APK here (manifest file stuff)
+    # d : array of DalvikVMFormat objects; Corresponds to the DEX file. We can obtain classes, methods, strings etc.
+    # from here.
+    # dx : Analysis object; Contains special classes, which link information about classes.dex and can handle multiple
+    # DEX files. (Extension of d object basically)
+    a, d, dx = AnalyzeAPK("./apks/WhatsApp.apk")
     analyzeAPK(a)
     analyzeDex(d, dx)
-
+    run_contextual()
 
 main()
