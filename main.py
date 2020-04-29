d5a82aaf924f8166d1515b4d231ffbbd4b8348b7
YonaBMoreda <y.moreda@student.rug.nl>
2020-04-29 15:08:41 +0200

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from androguard.misc import AnalyzeAPK
 from androguard.core.analysis import analysis
+from contextual_feat_extraction import run
 
 def main():
-    a, d, dx = AnalyzeAPK("./apks/WhatsApp.apk")
-    print(a.get_permissions())
-    #a.get_activities()
-    #a.get_intent_filters()
-
+    # a, d, dx = AnalyzeAPK("./apks/WhatsApp.apk")
+    # print(a.get_permissions())
+    # a.get_activities()
+    # a.get_intent_filters()
+    run()
 
 main()
